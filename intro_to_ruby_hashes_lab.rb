4308a12a473cffa74e7fcd5262d9ddf0a2247013
Spencer Smith <smithspencer817@gmail.com>
2020-05-04 15:45:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -63,6 +63,9 @@ def monopoly_with_fourth_tier
   monopoly[:railroads][:names][:shortline_railroad] = {}
   
   monopoly[:railroads][:names][:reading_railroad][:mortgage_value] = 100
+  monopoly[:railroads][:names][:pennsylvania_railroad][:mortgage_value] = 200
+  monopoly[:railroads][:names][:b_and_o_railroad][:mortgage_value] = 400
+  monopoly[:railroads][:names][:shortline_railroad][:mortgage_value] = 800
   
   monopoly
 
